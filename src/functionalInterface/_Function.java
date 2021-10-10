01fdef4f20b963d09aedfad278a1c7535b5612e2
Mrinal Gupta <mrinalgupta1097@gmail.com>
2021-10-11 00:30:16 +0530

    Function Chaining2

--- src/functionalInterface/_Function.java
+++ src/functionalInterface/_Function.java
@@ -6,8 +6,8 @@ public class _Function {
   public static void main(String[] args) {
     //
     /*
-    * Function functional interface takes an input and produces a result.
-    * Function<T,R> func;*/
+     * Function functional interface takes an input and produces a result.
+     * Function<T,R> func;*/
 
     int increment = increment(0);
     System.out.println(increment);
@@ -15,17 +15,20 @@ public class _Function {
     int increment1 = incrementByOneFunction.apply(1);
     System.out.println(increment1);
 
-    System.out.println(incrementByOneFunction.andThen(multiplyByTen).apply(1));//function chaining
-
+    System.out.println(
+        incrementByOneFunction
+            .andThen(multiplyByTen)
+            .andThen(squareIt)
+            .apply(1)); // function chaining
   }
 
-  static int increment(int number){
-    return number +1;
-
+  static int increment(int number) {
+    return number + 1;
   }
-//  above increment cam be written as below by using Function
-  static Function<Integer,Integer> incrementByOneFunction = number -> ++number;
+  //  above increment cam be written as below by using Function
+  static Function<Integer, Integer> incrementByOneFunction = number -> ++number;
 
-  static Function<Integer,Integer> multiplyByTen = number ->  number*10;
+  static Function<Integer, Integer> multiplyByTen = number -> number * 10;
 
+  static Function<Integer, Integer> squareIt = number -> number * number;
 }
