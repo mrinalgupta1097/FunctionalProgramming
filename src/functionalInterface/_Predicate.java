ea8e0f7626e520fb4245db8e64d0f24334bbe864
Mrinal Gupta <mrinalgupta1097@gmail.com>
2021-10-11 18:17:48 +0530

    Predicate Joining Code Demo

--- src/functionalInterface/_Predicate.java
+++ src/functionalInterface/_Predicate.java
@@ -9,6 +9,9 @@ public class _Predicate {
     System.out.println(isPhoneNUmberValid("09000000000"));
     //    Predicate Style - single argument boolean valued function
     System.out.println("Predicate Demo : " + isPhoneNumberValidPredicate.test("07000000000"));
+    System.out.println(
+        "Predicate Joining : "
+            + isPhoneNumberValidPredicate.and(containsNumber3).test("07000300000"));
   }
   // Plain Java Style
   static boolean isPhoneNUmberValid(String phoneNumber) {
@@ -18,4 +21,6 @@ public class _Predicate {
   //  Predicate Style
   static Predicate<String> isPhoneNumberValidPredicate =
       phoneNumber -> phoneNumber.startsWith("07") && phoneNumber.length() == 11;
+
+  static Predicate<String> containsNumber3 = phoneNumber -> phoneNumber.contains("3");
 }
