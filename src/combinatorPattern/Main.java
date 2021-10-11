1c06114f20d6a019ba08cdb82111a2c34b4e4a7f
Mrinal Gupta <mrinalgupta1097@gmail.com>
2021-10-12 00:26:56 +0530

    Combinator Pattern Plain

---
+++ src/combinatorPattern/Main.java
@@ -0,0 +1,14 @@
+package combinatorPattern;
+
+import java.time.LocalDate;
+
+public class Main {
+  public static void main(String[] args) {
+    //
+    Customer customer =
+        new Customer("ALice",
+                "alice@gmail.com",
+                "+919634431374",
+                LocalDate.of(2000, 1, 1));
+  }
+}
